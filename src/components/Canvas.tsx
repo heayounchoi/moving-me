1c0a90f5aff9adac1bd0896b1047bb78a9efe70b
heayounchoi <heayounchoi@gmail.com>
2023-01-10 16:36:47 +0900

    FEAT: 이벤트 바인딩 및 바인딩 해제 추가

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,6 @@
 import useCanvas from "../hooks/useCanvas";
 import Character from "../character/Character";
+import { useEffect } from "react";
 
 const Canvas = () => {
   let character: Character | null = null;
@@ -8,7 +9,15 @@ const Canvas = () => {
     canvas.height = window.innerHeight;
     canvas.style.background = "pink";
     character = new Character(canvas);
+    document.addEventListener("keydown", character.handleArrowKeyDown());
   });
+
+  useEffect(() => {
+    return () => {
+      character &&
+        document.removeEventListener("keydown", character.handleArrowKeyDown());
+    };
+  }, []);
 
   return (
     <div>

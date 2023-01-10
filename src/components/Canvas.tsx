b303b29d8667f56c57041ed36d5370076ae11f8a
heayounchoi <heayounchoi@gmail.com>
2023-01-10 16:25:33 +0900

    FEAT: 캔버스에 캐릭터 띄우기

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,10 +1,13 @@
 import useCanvas from "../hooks/useCanvas";
+import Character from "../character/Character";
 
 const Canvas = () => {
+  let character: Character | null = null;
   const canvasRef = useCanvas((canvas) => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.background = "pink";
+    character = new Character(canvas);
   });
 
   return (
